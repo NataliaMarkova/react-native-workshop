33ab42df993b84cda2ce53c636fb3d2178920532
Natalia Markova <Natalia.Markova@travisperkins.co.uk>
2019-02-25 17:42:13 +0200

    [Homework] : Task 4 - Implement ability to choose a media

--- android/app/src/main/java/com/workshoprn/MainApplication.java
+++ android/app/src/main/java/com/workshoprn/MainApplication.java
@@ -3,6 +3,7 @@ package com.workshoprn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -23,7 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(), new RNGestureHandlerPackage()
+          new MainReactPackage(),
+            new ImagePickerPackage(),
+            new RNGestureHandlerPackage()
       );
     }
 
--- screens/ThirdScreen.js
+++ screens/ThirdScreen.js
@@ -1,12 +1,84 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import { TouchableOpacity, Image, Text } from 'react-native';
+import sharedStyle from '../shared/style';
+import ImagePicker from 'react-native-image-picker';
 
-export default class SecondScreen extends React.Component {
-    render() {
-        return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-            <Text>Third Screen</Text>
-        </View>      
-        );
-    }
+const mediaPlaceholder = require('../images/media-placeholder.jpg');
+
+export default class ThirdScreen extends React.Component {
+
+  state = {
+    sourceUri: null,
+    mediaType: 'image'
+  }
+
+  _chooseFile() {
+
+    const options = {
+      title: 'Select the perfect view',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images'
+      },
+      noData: true,
+      customButtons: [
+        {name: 'video', title: 'Take Video...'},
+        {name: 'video_library', title: 'Choose Video from library...'},
+      ]
+    };
+
+    const videoOptions = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'movies'
+      },
+      noData: true,
+      mediaType: 'video'
+    };
+
+    ImagePicker.showImagePicker(options,
+    (response) => {
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton === 'video') {
+        ImagePicker.launchCamera(videoOptions, this._processVideoSelection.bind(this));
+      } else if (response.customButton === 'video_library') {
+        ImagePicker.launchImageLibrary(videoOptions, this._processVideoSelection.bind(this));
+      } else {
+        this.setState({ sourceUri: response.uri });
+      }
+    });
+  };
+
+  _processVideoSelection(response) {
+    if (response.didCancel) {
+      console.log('User cancelled image picker');
+    } else if (response.error) {
+      console.log('ImagePicker Error: ', response.error);
+    } else {
+      this.setState({ sourceUri: response.uri });
+    }  
+  }
+
+  _renderMedia() {
+    var mediaSource = mediaPlaceholder;
+    const sourceUri = this.state.sourceUri;
+    if (sourceUri) {
+      mediaSource = {uri: sourceUri};
+    } 
+    return (
+      <Image source={ mediaSource } style={sharedStyle.image} resizeMode={'contain'} />
+    );
+  };
+
+  render() {
+    return (
+    <TouchableOpacity style={sharedStyle.container} onPress={this._chooseFile.bind(this)}>
+      <Text style={sharedStyle.h3}>Upload your image or video here</Text>
+      {this._renderMedia()}
+    </TouchableOpacity>
+    );
+  }
 }
